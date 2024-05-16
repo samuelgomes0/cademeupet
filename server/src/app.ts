338de0e9a44f95738674cd59f772e7339d44caf2
Samuel Gomes <samuel_gomes26@hotmail.com>
2024-05-16 00:59:54 -0300

    📝 (app.ts): atualiza mensagem de log para indicar que o servidor está em execução 🐛 (app.ts): adiciona o host "0.0.0.0" para permitir conexões de rede externas

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,8 +3,9 @@ import server from "./server";
 server.listen(
   {
     port: 3000,
+    host: "0.0.0.0",
   },
   () => {
-    console.log(`Server listening on http://localhost:3000`);
+    console.log(`Server is running!`);
   }
 );
